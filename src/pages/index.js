d42ef692f1c417977305b8d6d49131a5852547ca
Ian Henderson <iancurtish@gmail.com>
2018-01-03 14:06:03 -0600

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,25 @@
 import React from 'react'
 import redditScreenshot from '../images/redditScreenshot.png'
+import resume from '../images/resume.pdf'
 import './index.css'
 
-const content = {
-  hero: {
-    header: "Ian Henderson",
-    subheader: "Software Engineer",
-    buttonLabel: "Yes, I'm available for hire",
-    learnMoreLabel: "Learn more about what I do"
+const expertises = [
+  {
+    iconClass: "fas fa-code",
+    title: "Front End",
+    summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
   },
-  me: {
-    intro: "My Expertise",
-    statement: "I've got chops in all phases of the design process.",
-    expertises: [
-      {
-        iconClass: "fas fa-terminal",
-        title: "Title 1",
-        summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
-      },
-      {
-        iconClass: "fab fa-codepen",
-        title: "Title 2",
-        summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
-      },
-      {
-        iconClass: "fab fa-node-js",
-        title: "Title 3",
-        summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
-      }
-    ]
+  {
+    iconClass: "fas fa-database",
+    title: "Back End",
+    summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
   },
-  personalProjects: {},
-  subFooter: {},
-  footer: {}
-}
+  {
+    iconClass: "fas fa-pencil-alt",
+    title: "???",
+    summary: "I collaborate with clients and peers to nurture and transform ideas into well thought out design specs. After all, that's where the majority of amazing user experiences start."
+  }
+]
 
 const IndexPage = () =>
   <div>
@@ -42,14 +28,23 @@ const IndexPage = () =>
       <div className='hero-container'>
         <div className='hero-content'>
           <div id='logo-container'>
-            <i id='hero-logo' className='fas fa-code'></i>
+            <i id='hero-logo' className='fab fa-grav'></i>
           </div>
-          <h1 id='hero-header'>{content.hero.header}</h1>
-          <p id='hero-subheader'>{content.hero.subheader}</p>
-          <a href='#'><button id='hero-button'>{content.hero.buttonLabel}</button></a>
+          <h1 id='hero-header'>
+            Ian Henderson
+          </h1>
+          <p id='hero-subheader' className='statement'>
+            Software Engineer
+          </p>
+          <a href='#sub-footer'>
+            <button id='hero-button'>
+              Yes, I'm available for hire
+            </button>
+          </a>
           <div className='learn-more-container'>
-            <a id='learn-more-link' href='#'>
-              {content.hero.learnMoreLabel}<br />
+            <a id='learn-more-link' href='#me'>
+              Learn more about what I do
+              <br />
               <i id='learn-more-chevron' className='fas fa-chevron-down'></i>
             </a>
           </div>
@@ -57,15 +52,19 @@ const IndexPage = () =>
       </div>
     </div>
 
-    {/* Me */}
+    {/* Expertise */}
     <section id='me'>
       <div className='container'>
         <div id='me-header-container'>
-          <p className='intro'>{content.me.intro}</p>
-          <p className='statement'>{content.me.statement}</p>
+          <p className='intro'>
+            My Expertise
+          </p>
+          <p className='statement'>
+            Full Stack Development
+          </p>
         </div>
         <div>
-          {content.me.expertises.map((expertise, index) => 
+          {expertises.map((expertise, index) => 
             <div className='column'>
               <div className='expertise-block'>
                 <i className={`${expertise.iconClass} expertise-logo`}></i>
@@ -82,13 +81,17 @@ const IndexPage = () =>
     <section id='personal-projects'>
       <div className='container'>
         <div>
-          <p className='intro'>Personal Projects</p>
-          <p className='statement'>When I'm not freelancing, I'm working on digital products.</p>
+          <p className='intro'>
+            Personal Projects
+          </p>
+          <p className='statement'>
+            When I'm not freelancing, I'm working on digital products.
+          </p>
         </div>
         <div>
           <div className='expertise-block'>
             <a target='_blank' href='https://reddit-reactjs.herokuapp.com'>
-              <img className='screenshot-container' src={redditScreenshot} />
+              <img src={redditScreenshot} />
             </a>
             <p className='description-container'>
               <a target='_blank' href='https://github.com/ian-henderson/reddit'>Reddit Browser</a> is platform that helps independent distributors of Young Living essential oils grow their business through automated class promotion and prospect follow-up management.
@@ -102,9 +105,17 @@ const IndexPage = () =>
     <section id='sub-footer'>
       <div className='container'>
         <div>
-          <p className='intro'>Want to work together?</p>
-          <p className='statement'>I'm currently accepting...</p>
-          <a href='mailto:iancurtish@gmail.com?subject=Work'><button>get started</button></a>
+          <p className='intro'>
+            Want to work together?
+          </p>
+          <p className='statement'>
+            I'm currently on the job hunt; feel free to reach out.
+          </p>
+          <a href='mailto:iancurtish@gmail.com?subject=Work'>
+            <button>
+              get started
+            </button>
+          </a>
         </div>
       </div>
     </section>
@@ -114,9 +125,11 @@ const IndexPage = () =>
       <div className='container'>
         <div>
           <div className='footer-logo-container'>
-            <i className='fas fa-code footer-logo'></i>
+            <i className='fab fa-grav footer-logo'></i>
           </div>
-          <p className='made-by-me'>Handmade by me © 2017.</p>
+          <p className='made-by-me'>
+            Handmade by me © 2018.
+          </p>
           <div className='social-logos-container'>
             {/* GitHub */}
             <a target='_blank' href='https://github.com/ian-henderson'>
@@ -125,6 +138,10 @@ const IndexPage = () =>
             {/* LinkedIn */}
             <a target='_blank' href='https://www.linkedin.com/in/iancurtish/'>
               <i className='fab fa-linkedin social-logo'></i>
+            </a>
+            {/* Resume */}
+            <a target='_blank' href={resume}>
+              <i className='fas fa-file-alt'></i>
             </a>
             {/* Twitter */}
             <a target='_blank' href='https://twitter.com/iancurtish'>
