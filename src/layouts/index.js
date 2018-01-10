1e93e12893e9d64c88e07c169cd0b28efb01cdab
Ian Henderson <iancurtish@gmail.com>
2018-01-10 14:39:52 -0600

    added google analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,6 +10,15 @@ const TemplateWrapper = ({ children }) => (
       <title>Ian Henderson | Software Engineer</title>
       <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"/>
       <link rel='shortcut icon' type='image/x-icon' href={favicon} />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112309795-1"></script>
+    {/* Google Analytics */}
+    <script>{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-112309795-1');
+    `}</script>
     </Helmet>
     {children()}
   </div>
