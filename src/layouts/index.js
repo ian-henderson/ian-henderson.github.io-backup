e10b4173d069fabd0672ae4eeafa4847e5140555
Ian Henderson <iancurtish@gmail.com>
2018-08-03 19:55:51 -0500

    added gTag & gAnalytics 😈

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,31 +4,41 @@ import Helmet from 'react-helmet'
 import favicon from '../images/favicon.ico'
 import './index.css'
 
+const gTagScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-112309795-1');
+`;
+
+const gAnalyticsScript = `
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', 'UA-112309795-1', 'auto');
+  ga('send', 'pageview');
+`
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
       <title>Ian Henderson | Software Engineer</title>
+
       <script
         defer
         src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"
       />
       <link rel="shortcut icon" type="image/x-icon" href={favicon} />
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-112309795-1"
-      />
-      {/* Google Analytics */}
-      <script>{`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
 
-      gtag('config', 'UA-112309795-1');
-    `}</script>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script>{gTagScript}</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112309795-1" />
+
+      {/* Google Analytics */}
+      <script>{gAnalyticsScript}</script>
+      <script async src="https://www.google-analytics.com/analytics.js" />
     </Helmet>
     {children()}
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
